d605616288b3ff5643954587bf8b4f3efc54bbef
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:11 -0500

    xml commenting

--- src/Soenneker.Bunny.HttpClients/BunnyOpenApiHttpClient.cs
+++ src/Soenneker.Bunny.HttpClients/BunnyOpenApiHttpClient.cs
@@ -46,11 +46,18 @@ public sealed class BunnyOpenApiHttpClient : IBunnyOpenApiHttpClient
             }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(BunnyOpenApiHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(BunnyOpenApiHttpClient));
